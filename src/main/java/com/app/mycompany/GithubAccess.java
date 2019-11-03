f8abe4be6010a53a718be4ac5d17678b6108797d
claire-1 <claire8991@gmail.com>
2019-11-03 22:28:10 +0000

    WIP getting all comments for all issues
    Right now, just gets all closed issues.

--- src/main/java/com/app/mycompany/GithubAccess.java
+++ src/main/java/com/app/mycompany/GithubAccess.java
@@ -1,7 +1,10 @@
 package com.app.mycompany;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueState;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
@@ -13,17 +16,30 @@ public class GithubAccess {
     private GHRepository repo;
 
     public GithubAccess(String repo) {
-        
+
         try {
             GitHub github = GitHub.connect();
             this.repo = github.getRepository(repo);
         } catch (IOException e) {
             // TODO Auto-generated catch block
-            throw new RuntimeException("GithubAccess|could not get repo", e); 
+            throw new RuntimeException("GithubAccess|could not get repo", e);
         }
     }
 
     public GHRepository getRepo() {
         return repo;
     }
+
+    public List<GHIssue> getClosedIssues() {
+        try {
+            return repo.getIssues(GHIssueState.CLOSED);
+        } catch (IOException e) {
+            throw new RuntimeException("GithubAccess|could not get issues", e);
+        }
+    }
+
+    // TODO get all comments for an issue in a repo
+    // https://github.com/github-api/github-api/blob/master/src/main/java/org/kohsuke/github/GHIssue.java#L287
+    // TODO then need to classify them as closed or resolved using spam classification algorithm
+
 }
--- src/test/java/com/app/mycompany/GithubAccessTest.java
+++ src/test/java/com/app/mycompany/GithubAccessTest.java
@@ -2,7 +2,11 @@ package com.app.mycompany;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
+import org.kohsuke.github.GHIssue;
 import org.kohsuke.github.GHRepository;
 
 /**
@@ -17,6 +21,18 @@ public class GithubAccessTest {
         GithubAccess github = new GithubAccess("claire-1/github-metrics");
         GHRepository repo = github.getRepo();
         assertEquals(repo.getName(), "github-metrics");
+    }
+
+    @Test
+    public void givenRepoWithClosedIssuesShouldIssues() {
+        List<GHIssue> expected = new LinkedList<>();
+       // expected.add("");
+       // TODO need to put an issue in here if want this test in the long run
+
+        GithubAccess github = new GithubAccess("claire-1/github-metrics");
+        List<GHIssue> actual = github.getClosedIssues();
+        
+        assertEquals(expected, actual); 
     }
 
 
