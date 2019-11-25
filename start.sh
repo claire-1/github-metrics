81c3c6210f86668b1ba121618308af455307beae
claire-1 <claire8991@gmail.com>
2019-11-25 20:02:31 +0000

    Copied and changed for bubble display from csv

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ mvn clean package
 mvn exec:java -Dexec.mainClass=com.app.mycompany.GithubAccess #-Djava.awt.headless=true
 
 ret=$?
-if [ $(ret) -ne 0 ]; then
+if [ ($ret) -ne 0 ]; then
         echo "There was an error. Not starting webserver."
 else
         http-server ./display -p 8080 -c-1
