3478540f093e02c7b74c817bde7cdedf470c0324
claire-1 <claire8991@gmail.com>
2019-11-27 12:26:16 +0000

    Remove extra table in database

--- mysql-dump/comments.sql
+++ mysql-dump/comments.sql
@@ -1,11 +1,5 @@
 CREATE DATABASE IF NOT EXISTS storage;
 USE storage;
-CREATE TABLE IF NOT EXISTS comments (
-    relatedIssueUrl VARCHAR(2083),
-    dateIssueClosed date,
-    content text
-);
-
 CREATE TABLE IF NOT EXISTS classifierResults (
     relatedIssueUrl VARCHAR(2083),
     dateIssueClosed date,
